97b0de4a4e4166feffecc0f4ce1bc5d3da0a386a
GephestUA <gephestua@gmail.com>
2021-05-28 19:29:14 +0300

    added Switch to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import ProfileContainer from './components/Profile/ProfileContainer'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import UsersContainer from './components/Users/UsersContainer'
 import HeaderContainer from './components/Header/HeaderContainer'
 import LoginPage from './components/Login/Login'
@@ -15,6 +15,7 @@ const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsCo
 
 function App(props) {
   useEffect(() => props.initializeApp())
+  console.log(1)
   if (!props.initialized) return <Preloader />
   return (
     <BrowserRouter>
@@ -22,10 +23,13 @@ function App(props) {
         <HeaderContainer />
         <Navbar></Navbar>
         <div className="app-wrapper-content">
-          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
-          <Route path="/users" render={() => <UsersContainer />} />
-          <Route path="/login" render={() => <LoginPage />} />
+          <Switch>
+            <Route exact path="/" render={() => <ProfileContainer />} />
+            <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+            <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+            <Route path="/users" render={() => <UsersContainer />} />
+            <Route path="/login" render={() => <LoginPage />} />
+          </Switch>
         </div>
       </div>
     </BrowserRouter>
